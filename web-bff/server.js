cc5e227f3fce0e34c5640c91a25f345015c22150
gaboserver <gabgonlo05@gmail.com>
2025-11-30 05:30:17 +0000

    wrong server.js

--- web-bff/server.js
+++ web-bff/server.js
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const hostname = process.env.BASE_URL || '127.0.0.1';
+const hostname = process.env.BASE_URL || "0.0.0.0"
 
 const options = {
   key: readFileSync(new URL("./ssl/localhost.key", import.meta.url)),
   cert: readFileSync(new URL("./ssl/localhost.crt", import.meta.url)),
 };
 
-const port = process.env.PORT || 443;
+const port = process.env.PORT || 3443;
 
 const server = createServer(options, (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
@@ -19,6 +19,6 @@ const server = createServer(options, (req, res) => {
 });
 
 
-server.listen(port, hostname, () => {
-  console.log(`Server running on https://${hostname}:${port} AND https://127.0.0.1:${port}`);
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on https://${hostname}:${port}`);
 });
